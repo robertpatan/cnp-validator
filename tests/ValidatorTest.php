ad932be132f6667bc4467e9c208ccb656f7bf761
Robert Patan <robert@webcangroup.ch>
2020-05-25 22:01:39 +0300

    added more tests

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -30,4 +30,19 @@ final class ValidatorTest extends \PHPUnit\Framework\TestCase
         $this->assertFalse(\Src\CnpValidator::validate('2900517016312'));
         
     }
+    
+    public function testNonNumeric(): void
+    {
+        $this->assertFalse(\Src\CnpValidator::validate('29a05s7016311'));
+    }
+    
+    public function testMoreThan13(): void
+    {
+        $this->assertFalse(\Src\CnpValidator::validate('29005170163123'));
+    }
+    
+    public function testLessThan13(): void
+    {
+        $this->assertFalse(\Src\CnpValidator::validate('290051701631'));
+    }
 }
